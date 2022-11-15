4fa9cc64a32c17516a0429e56c856d877cc67a6a
Todd Albert <todd@bocacode.com>
2022-11-15 14:34:37 -0500

    using context for language

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
-import { useState } from 'react'
+import { useState, createContext } from 'react'
+import Details from './components/Details'
 import English from './lang/en.json'
 import Spanish from './lang/es.json'
+import French from './lang/fr.json'
 import './App.css'
 
 const locale = navigator.language || 'en';
 
-const _lang = (locale==='es') ? Spanish : English;
+const _lang = (locale === 'es')
+  ? Spanish
+  : (locale === 'fr')
+    ? French
+    : English;
+
+export const LanguageContext = createContext()
 
 function App() {
   const [lang, setLang] = useState(_lang)
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <button onClick={() => setLang(English)}>EN</button>
-          <button onClick={() => setLang(Spanish)}>ES</button>
-        </div>
-        <h1>{lang["app.header"]}</h1>
-        <p>{lang["app.subhead"]}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {lang["app.cta"]}
-        </a>
-      </header>
-    </div>
+    <LanguageContext.Provider value={{ lang }}>
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <button onClick={() => setLang(English)}>EN</button>
+            <button onClick={() => setLang(Spanish)}>ES</button>
+            <button onClick={() => setLang(French)}>FR</button>
+          </div>
+          <h1>{lang["app.header"]}</h1>
+          <p>{lang["app.subhead"]}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {lang["app.cta"]}
+          </a>
+          <Details />
+        </header>
+      </div>
+    </LanguageContext.Provider>
   );
 }
 
