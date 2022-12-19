26bc8bd92e6beb3adbbc1f2454628931ff824aa5
Todd Albert <todd@bocacode.com>
2022-12-19 10:22:11 -0500

    added bug

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -5,7 +5,7 @@ export default function Details(){
   const { lang, English } = useContext(LanguageContext)
   return (
     <section>
-      <h3>{lang["details.header"] || English["details.header"]}</h3>
+      <h3>{lang["details"]["header"] || English["details"]["header"]}</h3>
       <p>{lang["details.text"] || English["details.text"]}</p>
     </section>
   )
